3684f286681a8b4bd7ed584b6a40241c72366a02
Evgeny Danilenko <6655321@bk.ru>
2022-07-15 16:35:06 +0300

    initial

---
+++ batch.go
@@ -0,0 +1,50 @@
+// You can edit this code!
+// Click here and start typing.
+package batch
+
+import (
+	"sync"
+)
+
+type Batch[T any] struct {
+	Values []T
+	m      sync.RWMutex
+}
+
+func NewBatch[T any](values ...T) *Batch[T] {
+	return &Batch[T]{values, sync.RWMutex{}}
+}
+
+func (b *Batch[T]) Apply(fns ...func(params ...T)) {
+	var wg sync.WaitGroup
+	for _, fn := range fns {
+		fn := fn
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			fn(b.Values...)
+		}()
+	}
+
+	wg.Wait()
+}
+
+func (b *Batch[T]) NewWrite(fn func(params ...T)) func(params ...T) {
+	return func(params ...T) {
+		b.m.Lock()
+		defer b.m.Unlock()
+
+		fn(params...)
+	}
+}
+
+func (b *Batch[T]) NewRead(fn func(params ...T)) func(params ...T) {
+	return func(params ...T) {
+		b.m.RLock()
+		defer b.m.RUnlock()
+
+		fn(params...)
+	}
+}
